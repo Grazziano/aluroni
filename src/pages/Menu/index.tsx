ee0435a84fb2d0adfb612fb07242a1c55f08113b
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-18 14:00:45 -0300

    Making the filters work

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -6,6 +6,7 @@ import Filters from './Filters';
 
 export default function Menu() {
   const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState<number | null>(null);
 
   return (
     <main>
@@ -21,7 +22,7 @@ export default function Menu() {
         <h3 className={styles.menu__title}>Menu</h3>
         <Search search={search} setSearch={setSearch} />
         <div className={styles.menu__filters}>
-          <Filters />
+          <Filters filter={filter} setFilter={setFilter} />
         </div>
       </section>
     </main>
