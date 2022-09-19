68f5db4b948fd29af6aa55b8102cd6fb9535b813
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-19 14:03:53 -0300

    Theme style

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import styles from './Layout.module.scss';
+import stylesTheme from 'styles/Theme.module.scss';
 
 export default function Layout() {
   return (
@@ -7,7 +8,7 @@ export default function Layout() {
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
-      <div>
+      <div className={stylesTheme.container}>
         <Outlet />
       </div>
     </>
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,4 +1,5 @@
 import styles from './Menu.module.scss';
+import stylesTheme from 'styles/Theme.module.scss';
 import Search from './Search';
 import { useState } from 'react';
 import Filters from './Filters';
@@ -12,7 +13,7 @@ export default function Menu() {
 
   return (
     <section className={styles.menu}>
-      <h3 className={styles.menu__title}>Menu</h3>
+      <h3 className={stylesTheme.titulo}>Menu</h3>
       <Search search={search} setSearch={setSearch} />
       <div className={styles.menu__filters}>
         <Filters filter={filter} setFilter={setFilter} />
