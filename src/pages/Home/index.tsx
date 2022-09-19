76065c45f65612535d0b3d443811334f6b1315aa
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-19 16:03:36 -0300

    Finishing the home screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import menu from 'data/menu.json';
 import styles from './Home.module.scss';
 import stylesTheme from 'styles/Theme.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Home() {
   let recommendedDishes = [...menu];
@@ -20,6 +21,17 @@ export default function Home() {
             <button className={styles.recomendado__botao}>Ver mais</button>
           </div>
         ))}
+      </div>
+
+      <h3 className={stylesTheme.titulo}>Nossa casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro, 3185
+          <br />
+          <br />
+          Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Layout from 'components/Layout';
 import Navbar from 'components/Navbar';
+import About from 'pages/About';
 import Home from 'pages/Home';
 import Menu from 'pages/Menu';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -13,6 +14,7 @@ export default function AppRouter() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="menu" element={<Menu />} />
+            <Route path="about" element={<About />} />
           </Route>
         </Routes>
       </Router>
