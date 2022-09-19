ed7c949018a8a5c121d00d7eb7900e94f7656fbe
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-19 01:56:50 -0300

    Recommended dishes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,25 @@
-import menu from 'pages/Menu/Items/items.json';
+import menu from 'data/menu.json';
+import styles from './Home.module.scss';
 
 export default function Home() {
+  let recommendedDishes = [...menu];
+  recommendedDishes = recommendedDishes
+    .sort(() => 0.5 - Math.random())
+    .splice(0, 3);
+
   return (
     <section>
-      <h3>Recomendações da cozinha</h3>
-      <div></div>
+      <h3 className={styles.titulo}>Recomendações da cozinha</h3>
+      <div className={styles.recomendados}>
+        {recommendedDishes.map((item) => (
+          <div key={item.id} className={styles.recomendado}>
+            <div className={styles.recomendado__imagem}>
+              <img src={item.photo} alt={item.title} />
+            </div>
+            <button className={styles.recomendado__botao}>Ver mais</button>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
