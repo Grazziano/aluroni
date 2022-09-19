d6c80cc675dd26ef337dc8ccfa3c2e6fad362003
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-19 02:01:48 -0300

    Solving routes with js

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-// import Menu from './pages/Menu';
+import Menu from './pages/Menu';
 import Home from 'pages/Home';
+
+const componenteAtual = window.location.pathname === '/' ? <Home /> : <Menu />;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>{componenteAtual}</React.StrictMode>);
