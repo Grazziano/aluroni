f44cb657aab7c48b87999c4f858adfa2ce0874d1
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-19 16:39:18 -0300

    Creating NotFound

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Navbar from 'components/Navbar';
 import About from 'pages/About';
 import Home from 'pages/Home';
 import Menu from 'pages/Menu';
+import NotFound from 'pages/NotFound';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
@@ -17,6 +18,7 @@ export default function AppRouter() {
             <Route path="menu" element={<Menu />} />
             <Route path="about" element={<About />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
