189f30aabf6a472057abd994f1e2297a9e51e386
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-19 13:31:25 -0300

    Default page Layout

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,19 @@
+import Layout from 'components/Layout';
 import Navbar from 'components/Navbar';
 import Home from 'pages/Home';
 import Menu from 'pages/Menu';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styles from './Routes.module.scss';
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
         <Navbar />
-
-        <header className={styles.header}>
-          <div className={styles.header__text}>A casa do código e da massa</div>
-        </header>
-
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="menu" element={<Menu />} />
+          </Route>
         </Routes>
       </Router>
     </main>
