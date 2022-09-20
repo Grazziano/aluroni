5f67c226780f59f8b0101ee248e7748e94abba3d
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-19 22:45:15 -0300

    Back button

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,8 +2,11 @@ import styles from './NotFound.module.scss';
 import stylesTheme from 'styles/Theme.module.scss';
 import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
 import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
     <div
       className={classNames({
@@ -11,6 +14,9 @@ export default function NotFound() {
         [stylesTheme.container]: true,
       })}
     >
+      <div className={styles.voltar}>
+        <button onClick={() => navigate(-1)}>{'< Voltar'}</button>
+      </div>
       <NotFoundImage />
     </div>
   );
